5afbb02b6a09e0600689418e9a01ae9841ce2870
giologizt <80735653+giologizt@users.noreply.github.com>
2021-08-23 16:51:49 +0800

    added login and signup

--- Clink/app/src/main/java/com/mobdeve/group19/clink/MainActivity.java
+++ Clink/app/src/main/java/com/mobdeve/group19/clink/MainActivity.java
@@ -2,7 +2,9 @@ package com.mobdeve.group19.clink;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    private void addTweet() {
+        this.fabTweet = findViewById(R.id.editBtn);
+        this.fabTweet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (MainActivity.this, AddTweetActivity.class);
+
+                Launcher.launch(intent);
+            }
+        });
+    }
+
+
+
 }
