2a11b79c1cff2ea713df5e3cdbc3da830b27d23d
giologizt <80735653+giologizt@users.noreply.github.com>
2021-08-26 16:13:44 +0800

    minor updates

--- Clink/app/src/main/java/com/mobdeve/group19/clink/AdapterRecipes.java
+++ Clink/app/src/main/java/com/mobdeve/group19/clink/AdapterRecipes.java
@@ -1,7 +1,38 @@
 package com.mobdeve.group19.clink;
 
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class AdapterRecipes extends RecyclerView.Adapter<> {
+import java.util.ArrayList;
 
+public class AdapterRecipes extends RecyclerView.Adapter<ViewHolderRecipes> {
+    private ArrayList<Recipe> data;
+
+    public AdapterRecipes(ArrayList<Recipe> data) {this.data = data; }
+
+    @NonNull
+    @org.jetbrains.annotations.NotNull
+    @Override
+    public ViewHolderRecipes onCreateViewHolder(@NonNull @org.jetbrains.annotations.NotNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.RecipesActivity, parent, false);
+        ViewHolderRecipes ViewHolderRecipes  = new ViewHolderRecipes(v);
+
+        return ViewHolderRecipes;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull @org.jetbrains.annotations.NotNull ViewHolderRecipes holder, int position) {
+        holder.setNameTv(data.get(position).getName());
+        holder.setTimeTv(data.get(position).getTime());
+        holder.setCocktailIv(data.get(position).getImageId());
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.size();
+    }
 }
