be8ba4ce1442547b26c8a390eb7fddc157d6a63c
dychosenone <50646953+dychosenone@users.noreply.github.com>
2021-09-05 14:12:33 +0800

    fixed recipe page scroll view

--- Clink/app/src/main/java/com/mobdeve/group19/clink/ViewHolderFeedback.java
+++ Clink/app/src/main/java/com/mobdeve/group19/clink/ViewHolderFeedback.java
@@ -7,8 +7,6 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import org.jetbrains.annotations.NotNull;
-
 public class ViewHolderFeedback extends RecyclerView.ViewHolder {
     private TextView usernameTv, commentTv;
     private ImageView userIv;
