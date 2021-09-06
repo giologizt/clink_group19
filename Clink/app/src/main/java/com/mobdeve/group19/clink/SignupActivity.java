43966a6a75ba24433ea0e6e5fb667149228ac076
JulCL123 <77001355+JulCL123@users.noreply.github.com>
2021-09-07 00:04:58 +0800

    sign up

--- Clink/app/src/main/java/com/mobdeve/group19/clink/SignupActivity.java
+++ Clink/app/src/main/java/com/mobdeve/group19/clink/SignupActivity.java
@@ -8,17 +8,36 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.SimpleExpandableListAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.mobdeve.group19.clink.model.ApiHelper;
+import com.mobdeve.group19.clink.model.CustomCallback;
+import com.mobdeve.group19.clink.model.Message;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+import java.util.concurrent.ExecutorService;
 
 public class SignupActivity extends AppCompatActivity {
 
     private Button btnSignup;
-    private TextView etFullname, etEmail, etPassword, etBirthDay, etBirthMonth, etBirthYear;
+    private TextView etFullname, etEmail, etPassword, etUsername, etBirthDay, etBirthMonth, etBirthYear;
 
-    private String fullname, email, password;
+    private String fullname, email, password, username;
     private int day, month, year;
+
+    ApiHelper helper;
+
+    ExecutorService executorService;
 
 
     @Override
@@ -26,11 +45,14 @@ public class SignupActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
+        Intent intent = new Intent (SignupActivity.this, LoginActivity.class);
+
         btnSignup = findViewById(R.id.signupBtn);
 
         etFullname = findViewById(R.id.et_signupFullname);
         etEmail = findViewById(R.id.et_signupEmail);
         etPassword = findViewById(R.id.et_signupPassword);
+        etUsername = findViewById(R.id.et_signupUsername);
 
         etBirthDay = findViewById(R.id.et_signupDay);
         etBirthMonth = findViewById(R.id.et_signupMonth);
@@ -44,15 +66,61 @@ public class SignupActivity extends AppCompatActivity {
                 fullname = etFullname.getText().toString();
                 password = etPassword.getText().toString();
                 email = etEmail.getText().toString();
+                username = etUsername.getText().toString();
 
-                day = Integer.parseInt(etBirthDay.getText().toString());
-                month = Integer.parseInt(etBirthMonth.getText().toString());
-                year = Integer.parseInt(etBirthYear.getText().toString());
-
-                if(fullname.equals("") || password.equals("") || email.equals("")){
-
+                if(fullname.equals("") || password.equals("") || email.equals("") || username.equals("") || etBirthDay.getText().toString().equals("")
+                    || etBirthMonth.getText().toString().equals("") || etBirthYear.getText().toString().equals("")){
+                    Toast.makeText(getApplicationContext(), "Error: Please fill up all text fields.", Toast.LENGTH_SHORT).show();
                 } else {
 
+                    day = Integer.parseInt(etBirthDay.getText().toString());
+                    month = Integer.parseInt(etBirthMonth.getText().toString());
+                    year = Integer.parseInt(etBirthYear.getText().toString());
+
+                    Integer legalyear = (year + 18);
+                    Date now = Calendar.getInstance().getTime();
+
+                    String birthday =  etBirthMonth.getText().toString() + "/" + etBirthDay.getText().toString() + "/" + etBirthYear.getText().toString();
+                    String legalDate =  etBirthMonth.getText().toString() + "/" + etBirthDay.getText().toString() + "/" + legalyear.toString();
+
+                    SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy");
+                    SimpleDateFormat todayFormat = new SimpleDateFormat("MM/dd/yyyy", Locale.getDefault());
+                    Date today = null;
+                    Date legal = null;
+
+                    Log.d("MainActivity", now.toString());
+
+                    try {
+                        //today = dateFormat.parse(now.toString());
+                        legal = dateFormat.parse(legalDate);
+
+                        if(legal.compareTo(now) > 0){
+                            Toast.makeText(getApplicationContext(), "Error: You're not legally allowed to use this app.", Toast.LENGTH_SHORT).show();
+                        } else {
+                            executorService.execute(new Runnable(){
+                                @Override
+                                public void run() {
+                                    helper.register(username, fullname, email, birthday, password, new CustomCallback() {
+                                        @Override
+                                        public void success(Message message) {
+                                            if(message.getCode() == 400){
+                                                Toast.makeText(getApplicationContext(), "Error: " + message.getMessage(), Toast.LENGTH_SHORT).show();
+                                            } else {
+                                                Launcher.launch(intent);
+                                            }
+                                        }
+
+                                        @Override
+                                        public void failure(Throwable t) {
+                                            t.printStackTrace();
+                                        }
+                                    });
+                                }
+                            });
+                        }
+                    } catch (ParseException e) {
+                        e.printStackTrace();
+                    }
                 }
             }
         });
